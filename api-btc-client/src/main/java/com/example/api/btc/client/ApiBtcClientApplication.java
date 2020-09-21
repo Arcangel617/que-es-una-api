513e23b02a4d9dd0ab2f62cb2ba96d118d181cd7
Arcangel Andres Artigue <artigue_arcangel@hotmail.com>
2020-09-21 12:14:04 -0300

    feat: first commit

---
+++ api-btc-client/src/main/java/com/example/api/btc/client/ApiBtcClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.api.btc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBtcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBtcClientApplication.class, args);
+	}
+
+}
---
+++ api-btc-client/src/test/java/com/example/api/btc/client/ApiBtcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.btc.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBtcClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
