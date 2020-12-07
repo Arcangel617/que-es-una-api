f313bb9d0d562751332b8cdf258a65219c46073e
Arcangel Andres Artigue <artigue_arcangel@hotmail.com>
2020-12-07 17:32:32 -0300

    feat: upgrade dependencies

--- api-btc-client/src/main/java/com/example/api/btc/client/Application.java
+++ api-btc-client/src/main/java/com/example/api/btc/client/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class ApiBtcClientApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(ApiBtcClientApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
